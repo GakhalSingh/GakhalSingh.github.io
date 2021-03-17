39363b7a8d6e0b2f17ac08dab9e35d2b1a183c5c
fortnite <70706370+GakhalSingh@users.noreply.github.com>
2021-03-17 15:56:28 +0100

    Delete Portfolio Project HTML met bootstrap directory

--- Portfolio Project HTML met bootstrap/code.js
+++
@@ -1,9 +0,0 @@
-const NeonModeButton = document.getElementById('NeonModeButton');
-NeonModeButton.onclick = () => {
-    body.classList.replace('normalMode', 'neonMode');
-  };
-  
-function button(){
-  const bodyTheme = document.body;
-  bodyTheme.classList.toggle("neonMode");
-};
