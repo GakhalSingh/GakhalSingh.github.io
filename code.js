3c90fbab21d020e0f58ef0c3dac5ba101514e0d0
fortnite <70706370+GakhalSingh@users.noreply.github.com>
2021-03-24 14:07:45 +0100

    Geupdate versie van site met themas en rekentool

--- code.js
+++ code.js
@@ -1,9 +1,14 @@
-const NeonModeButton = document.getElementById('NeonModeButton');
-NeonModeButton.onclick = () => {
-    body.classList.replace('normalMode', 'neonMode');
-  };
-  
 function button(){
   const bodyTheme = document.body;
   bodyTheme.classList.toggle("neonMode");
+  setInterval(button, 6000);
+};
+const NeonModeButton = document.getElementById('NeonModeButton');
+NeonModeButton.onclick = () => {
+  body.classList.replace('normalMode', 'neonMode');
+};
+function dayToHour() {
+  let days = document.getElementById("numberDays").value;
+  document.getElementById("hours").innerHTML = days / 24;
+  console.log(days, hours);
 };
