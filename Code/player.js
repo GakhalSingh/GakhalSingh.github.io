44be2ca0c7d4d8d436263b98b23902670a608abd
Keren Blokpoel (REAL)[not fake] <70706370+GakhalSingh@users.noreply.github.com>
2021-06-28 10:15:54 +0200

    Last minute updates

---
+++ Code/player.js
@@ -0,0 +1,9 @@
+export default class Player {
+    constructor(name, symbol) {
+      //Make this work
+      this.name = name;
+      this.symbol = symbol;
+      this.points = 0;
+      this.level = 1;
+    };
+};
